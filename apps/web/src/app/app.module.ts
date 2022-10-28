cd16f2d84f6dc35ee62c8ae1eb4be0a7a79d97ad
Valery Melou <me@valerymelou.com>
2022-10-29 00:21:45 +0100

    chore: add @angular/material

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/web/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    CommonModule,
+    MatButtonModule
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule
+  ]
+})
+export class SharedModule { }
